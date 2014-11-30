c08895aa536fbf7e8c679dba5cc812725028db98
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2014-11-30 11:51:02 +0900

    Add command test

--- 2014/11/30/gocon_2014_autumn/double_test.go
+++ 2014/11/30/gocon_2014_autumn/double_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"os/exec"
 	"testing"
 )
 
@@ -10,6 +11,19 @@ func TestDouble(t *testing.T) {
 	stdout := new(bytes.Buffer)
 
 	Double(stdin, stdout)
+
+	if stdout.String() != "hoge\nhoge\n" {
+		t.Fatal("Not matched")
+	}
+}
+
+func TestCommand(t *testing.T) {
+	cmd := exec.Command("go", "run", "double.go")
+	cmd.Stdin = bytes.NewBufferString("hoge\n")
+	stdout := new(bytes.Buffer)
+	cmd.Stdout = stdout
+
+	_ = cmd.Run()
 
 	if stdout.String() != "hoge\nhoge\n" {
 		t.Fatal("Not matched")
